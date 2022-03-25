1713056736f6a0b6d60e52025621f8fb64472db6
leon lishenga <lishengaleon@gmail.com>
2022-03-25 18:02:12 +0300

    releases

--- src/main/java/com/kangaroo/interview/v1/requests/GeneralPagedRequest.java
+++ src/main/java/com/kangaroo/interview/v1/requests/GeneralPagedRequest.java
@@ -10,9 +10,6 @@ import javax.validation.constraints.NotNull;
 @Getter @Setter
 public class GeneralPagedRequest implements Serializable {
 
-    /**
-     *
-     */
     private static final long serialVersionUID = -6487671289690827077L;
 
     @NotNull(message = "Provide page")
